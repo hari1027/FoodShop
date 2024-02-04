86a89d68d77ce21398637f6a2fc8d3c8576e69ae
Hariharan Narayanan <hariharan.n@ozonetel.com>
2024-02-04 06:12:59 +0530

    changes in 4 feb

--- Components/mainScreen.jsx
+++ Components/mainScreen.jsx
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  PermissionsAndroid,
+  BackHandler,
 } from 'react-native';
 import React, {useEffect, useState, useContext} from 'react';
 import ShopCard from './shopCard';
@@ -18,6 +20,7 @@ import {AppwriteContext} from '../LoginComponents/appwrite/AppwriteContext';
 import axios from 'react-native-axios';
 import store from '../store';
 import Snackbar from 'react-native-snackbar';
+import Geolocation from '@react-native-community/geolocation';
 
 const MainScreen = ({navigation}) => {
   const dispatch = useDispatch();
@@ -58,6 +61,11 @@ const MainScreen = ({navigation}) => {
   const [singleShopView, setSingleShopView] = useState(false);
   const [singleShopDetails, setSingleShopDetails] = useState({});
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isNearByShops, setIsNearByShops] = useState(false);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [shopsWithLatAndLong, setShopsWithLatAndLong] = useState([]);
+  const [nearByShopsList, setNearByShopsList] = useState([]);
 
   const getShopDataById = async item => {
     try {
@@ -176,6 +184,199 @@ const MainScreen = ({navigation}) => {
     }
   };
 
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance;
+  }
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'App needs access to your location.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        Geolocation.getCurrentPosition(
+          position => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+            if (
+              position.coords.latitude !== null &&
+              position.coords.longitude !== null
+            ) {
+              geocodeShopAddresses();
+            }
+          },
+          error => console.log(error.message),
+          {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+        );
+      } else {
+        console.log('Location permission denied ');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const geocodeShopAddresses = async () => {
+    const geocodedShops = await Promise.all(
+      shopsList.map(async shop => {
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+            shop.shopAddress,
+          )}&key=AIzaSyC0bR8w93ONNL2ljxOe4CG4sM1--JcW9Zw`,
+        );
+        const data = await response.json();
+        console.log(data);
+        if (data.results && data.results.length > 0) {
+          const location = data.results[0].geometry.location;
+          return {...shop, lat: location.lat, lon: location.lng};
+        } else {
+          return null;
+        }
+      }),
+    );
+    console.log(geocodedShops);
+    setShopsWithLatAndLong(geocodedShops);
+    if (shopsWithLatAndLong.includes(null)) {
+      const filteredArray = shopsWithLatAndLong.filter(item => item !== null);
+      setShopsWithLatAndLong(filteredArray);
+    } else {
+      openDirection();
+    }
+  };
+
+  const openDirection = () => {
+    if (latitude !== null && longitude !== null) {
+      const currentLocation = {latitude, longitude};
+
+      const nearbyShops = shopsWithLatAndLong.filter(shop => {
+        if (
+          shop.lat !== undefined &&
+          shop.lon !== undefined &&
+          shop.lat !== null &&
+          shop.lon !== null
+        ) {
+          const distance = calculateDistance(
+            currentLocation.latitude,
+            currentLocation.longitude,
+            shop.lat,
+            shop.lon,
+          );
+          return distance <= 10;
+        }
+      });
+
+      setNearByShopsList(nearbyShops);
+    }
+  };
+
+  const toogleNearBy = async () => {
+    setIsNearByShops(!isNearByShops);
+    if (isNearByShops === false) {
+      requestLocationPermission();
+      if (nearByShopsList.length > 0) {
+        let nearByShopIds = [];
+        nearByShopsList.map(data => {
+          nearByShopIds.push(data._id);
+        });
+        const nearByShopsData = [];
+
+        try {
+          // Use Promise.all to await all promises
+          const responses = await Promise.all(
+            nearByShopIds.map(async item => {
+              try {
+                return await getShopDataById(item);
+              } catch (error) {
+                console.log(`Error: ${error}`);
+                // Return a default value or handle the error as needed
+                return null;
+              }
+            }),
+          );
+
+          // Filter out any null values (failed requests)
+          const successfulResponses = responses.filter(
+            response => response !== null,
+          );
+
+          // Do something with the successful responses
+          nearByShopsData.push(...successfulResponses);
+        } catch (error) {
+          console.log(`Error: ${error}`);
+        }
+
+        setShopsList(nearByShopsData);
+
+        let resp = nearByShopsData;
+        resp.map(async data => {
+          try {
+            const response = await axios.get(
+              `http://10.0.2.2:5000/getShopRatingAndFeedback/${data._id}`,
+            );
+            if (response.status === 200) {
+              console.log(
+                `Rating and Feedback of shop with Id:${data._id} have been fetched Successfully `,
+              );
+              let filteredArray = resp;
+              let individualData = response.data.shop.feedback;
+              let updatedShop = {
+                ...data,
+                avgRating: response.data.averageRating,
+                totalPeopleGivenRating: response.data.totalPeopleGivenRating,
+                totalPeopleGivenComments:
+                  response.data.totalPeopleGivenComments,
+              };
+              individualData.map(item => {
+                if (item.email === store.getState().main.uniqueEmailId) {
+                  updatedShop = {...updatedShop, yourRating: item.rating};
+                  if (item.name !== undefined && item.name !== null) {
+                    updatedShop = {...updatedShop, yourName: item.name};
+                  }
+                  if (item.comments !== undefined && item.comments !== null) {
+                    updatedShop = {
+                      ...updatedShop,
+                      yourComment: item.comments,
+                    };
+                  }
+                }
+              });
+              filteredArray = filteredArray.filter(
+                item => item._id !== data._id,
+              );
+              filteredArray.push(updatedShop);
+              resp = filteredArray;
+              setShopsList(filteredArray);
+            }
+          } catch (error) {
+            console.log(`${error}`);
+          }
+        });
+      } else {
+        setShopsList([]);
+      }
+    } else {
+      getShops();
+    }
+  };
+
   const getShops = async () => {
     try {
       let resp = await axios.get('http://10.0.2.2:5000/FoodShop');
@@ -281,6 +482,10 @@ const MainScreen = ({navigation}) => {
     setSingleShopDetails(shop);
   };
 
+  const handleExitApp = () => {
+    BackHandler.exitApp();
+  };
+
   return (
     <View style={styles.mainDiv}>
       {!addShopForm &&
@@ -310,9 +515,9 @@ const MainScreen = ({navigation}) => {
                     size={24}
                     color="#009688"
                     onPress={() => {
-                      isBookmarked
+                      isBookmarked || isNearByShops
                         ? Snackbar.show({
-                            text: 'Please DeSelect Saved Option and Try',
+                            text: 'Please DeSelect Saved and NearBy Option and Try',
                             duration: Snackbar.LENGTH_LONG,
                           })
                         : setAddShopForm(true);
@@ -331,9 +536,9 @@ const MainScreen = ({navigation}) => {
                     size={24}
                     color="#4caf50"
                     onPress={() =>
-                      isBookmarked
+                      isBookmarked || isNearByShops
                         ? Snackbar.show({
-                            text: 'Please DeSelect Saved Option and Try',
+                            text: 'Please DeSelect Saved and NearBy Option and Try',
                             duration: Snackbar.LENGTH_LONG,
                           })
                         : setUpdateShopForm(true)
@@ -352,9 +557,9 @@ const MainScreen = ({navigation}) => {
                     size={24}
                     color="red"
                     onPress={() =>
-                      isBookmarked
+                      isBookmarked || isNearByShops
                         ? Snackbar.show({
-                            text: 'Please DeSelect Saved Option and Try',
+                            text: 'Please DeSelect Saved and NearBy Option and Try',
                             duration: Snackbar.LENGTH_LONG,
                           })
                         : setDeleteShopForm(true)
@@ -372,9 +577,37 @@ const MainScreen = ({navigation}) => {
                     name="bookmark"
                     size={24}
                     color={isBookmarked ? 'green' : 'pink'}
-                    onPress={toggleBookmark}
+                    onPress={() =>
+                      isNearByShops
+                        ? Snackbar.show({
+                            text: 'Please DeSelect NearBy Option and Try',
+                            duration: Snackbar.LENGTH_LONG,
+                          })
+                        : toggleBookmark()
+                    }
                   />
                   <Text style={styles.buttonText}>Saved </Text>
+                </View>
+                <View
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                  }}>
+                  <Icon
+                    name="road"
+                    size={24}
+                    color={isNearByShops ? 'green' : 'yellow'}
+                    onPress={() =>
+                      isBookmarked
+                        ? Snackbar.show({
+                            text: 'Please DeSelect Saved Option and Try',
+                            duration: Snackbar.LENGTH_LONG,
+                          })
+                        : toogleNearBy()
+                    }
+                  />
+                  <Text style={styles.buttonText}>NearBy </Text>
                 </View>
                 <View
                   style={{
@@ -388,9 +621,9 @@ const MainScreen = ({navigation}) => {
                       size={24}
                       color="#007bff"
                       onPress={() =>
-                        isBookmarked
+                        isBookmarked || isNearByShops
                           ? Snackbar.show({
-                              text: 'Please DeSelect Saved Option and Try',
+                              text: 'Please DeSelect Saved and NearBy Option and Try',
                               duration: Snackbar.LENGTH_LONG,
                             })
                           : navigation.openDrawer()
@@ -414,13 +647,34 @@ const MainScreen = ({navigation}) => {
                     flexDirection: 'column',
                   }}>
                   <Icon
+                    name="close"
+                    size={24}
+                    color="red"
+                    onPress={() => {
+                      isBookmarked || isNearByShops
+                        ? Snackbar.show({
+                            text: 'Please DeSelect Saved and NearBy Option and Try',
+                            duration: Snackbar.LENGTH_LONG,
+                          })
+                        : handleExitApp();
+                    }}
+                  />
+                  <Text style={styles.buttonText}>Exit</Text>
+                </View>
+                <View
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                  }}>
+                  <Icon
                     name="sign-out"
                     size={24}
                     color="#900D09"
                     onPress={() =>
-                      isBookmarked
+                      isBookmarked || isNearByShops
                         ? Snackbar.show({
-                            text: 'Please DeSelect Saved Option and Try',
+                            text: 'Please UnSelect Saved and NearBy Option and Try',
                             duration: Snackbar.LENGTH_LONG,
                           })
                         : handleLogout()
